a419652e44b82c636a38051384d01d849a55c682
Sagar Jaglan <sagarjaglan0511@gmail.com>
2023-03-11 12:02:39 +0530

    new

--- pages/app/store.js
+++ pages/app/store.js
@@ -1,10 +1,13 @@
+import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
 //the global store
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     basket: basketReducer,
   },
   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 });
+
+export default store;
